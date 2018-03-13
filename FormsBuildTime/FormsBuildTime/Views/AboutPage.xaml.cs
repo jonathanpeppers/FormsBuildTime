e8f935d32425737c4ee26b030ee12a6127388840
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-03-13 09:53:55 -0500

    File -> New Project

---
+++ FormsBuildTime/FormsBuildTime/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsBuildTime.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
